f0189c94baf90fc3172659908f8ad044cf5b9efd
Matt Helm <mthelm85@gmail.com>
2018-10-12 06:20:06 -0500

    added function to get candles to Predictor

--- src/components/Predictor.vue
+++ src/components/Predictor.vue
@@ -1,11 +1,24 @@
 <template lang="html">
   <v-container>
-    Test
+    <v-btn @click="last3Candles">Candles</v-btn>
   </v-container>
 </template>
 
 <script>
+import * as tf from '@tensorflow/tfjs'
 export default {
+  data () {
+    return {
+
+    }
+  },
+
+  methods: {
+    async last3Candles () {
+      let res = await this.$candlesM4('EUR_USD', 0.5, 0, 'M4')
+      console.log(res)
+    }
+  }
 }
 </script>
 
